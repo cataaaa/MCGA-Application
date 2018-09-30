2d57039fed2aa69c5d56a563251a47911862f8a9
Cristian Diaz <diazcristian1990@hotmail.com>
2018-09-30 11:54:59 -0300

    Tag

--- MCGA.website/Controllers/HomeController.cs
+++ MCGA.website/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Constants.HomeController;
 
 namespace MCGA.website.Controllers
 {
@@ -10,21 +11,23 @@ namespace MCGA.website.Controllers
     //[Authorize]
     public class HomeController : Controller
     {
+        [Route("", Name = HomeControllerRoute.GetIndex)]
         public ActionResult Index()
         {
           
             var x = User.Identity.IsAuthenticated;
             var r = User.IsInRole("admin");
-            return View();
+            // return View();
+            return this.View(HomeControllerAction.Index);
         }
-
+        [Route("About", Name = HomeControllerRoute.GetAbout)]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        [Route("Contact", Name = HomeControllerRoute.GetContact)]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
